ca509020488e07ee9d15eaba5dcc528481136113
TristanKiel <143867@student.dcterra.nl>
2025-03-10 11:41:29 +0100

    verder met koppeltabel
    Ik heb drie functies voor mijn koppeltabel. Is alleen nog niet af.

--- app/koppeltabel.php
+++ app/koppeltabel.php
@@ -1,13 +1,25 @@
 <?php
 
-    public function insert(){
-        $stmt = $this->conn->prepare("INSERT INTO user_games");
-        $stmt->execute();
-    }
+    class koppeltabel{
+        
+        public function insert(){
+            $stmt = $this->conn->prepare("INSERT INTO user_games (user_id, game_id");
+            $stmt->execute();
+        }
 
-    public function select(){
-        $stmt = $this->->prepare("SELECT games.title FROM games");
-        $stmt->execute();
+        public function selectGames(){
+            $stmt = $this->conn->prepare("SELECT games.title FROM games");
+            $stmt = $this->conn->prepare("INNER JOIN user_games ON games.id = user_games.game_id");
+            $stmt = $this->conn->prepare("WHERE user_games.game_id = ");
+            $stmt->execute();
+        }
+
+        public function selectUsers(){
+            $stmt = $this->conn->prepare("SELECT user.username FROM games");
+            $stmt = $this->conn->prepare("INNER JOIN user_games ON user.id = user_games.user_id");
+            $stmt = $this->conn->prepare("WHERE user_games.game_id = ");
+            $stmt->execute();
+        }
     }
 
 ?> 
