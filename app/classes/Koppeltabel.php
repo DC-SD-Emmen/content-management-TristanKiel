d2996be07b30b9fcc3474c8673f6c02dd9a2bb23
TristanKiel <143867@student.dcterra.nl>
2025-03-11 10:19:28 +0100

    classes/koppeltabel.php
    Ik heb het koppeltabel aan de classes toegevoegd en een paar foutjes verbeterd.

---
+++ app/classes/Koppeltabel.php
@@ -0,0 +1,33 @@
+<?php
+
+    class Koppeltabel {
+
+        public function insert(){
+            $stmt = $this->conn->prepare("INSERT INTO user_games (user_id, game_id) VALUES (20, 27)");
+            $stmt->execute();
+        }
+
+        public function selectGames($user_id){
+            $stmt = $this->conn->prepare("SELECT games.title
+                                        FROM games
+                                        INNER JOIN user_games ON games.id = user_games.game_id
+                                        WHERE user_games.user_id = :user_id;");
+            $stmt->bindParam(':user_id', $user_id);
+                                    
+            $stmt->execute();
+            $stmtFetchAll = $stmt->fetch(PDO:: FETCH_ASSOC);
+            return $stmtFetchAll
+        }
+
+        public function selectUsers($game_id){
+            $stmt = $this->conn->prepare("SELECT user.username
+                                        FROM games
+                                        INNER JOIN user_games ON user.id = user_games.user_id
+                                        WHERE user_games.game_id = :game_id;");
+            $stmt->execute();
+            $stmtFetchAll = $stmt->fetch(PDO:: FETCH_ASSOC);
+            return $stmtFetchAll
+        }
+    }
+
+?>
