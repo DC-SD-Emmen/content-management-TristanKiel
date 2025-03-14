ba24672f5c5f65f3fa70262947117a000cf13308
TristanKiel <143867@student.dcterra.nl>
2025-03-14 12:51:25 +0100

    Select functie fixen
    I.P.V image heb ik nu alles geselecteerd.

--- app/classes/Koppeltabel.php
+++ app/classes/Koppeltabel.php
@@ -14,7 +14,7 @@
         }
 
         public function selectGames($user_id){
-            $stmt = $this->conn->prepare("SELECT games.image
+            $stmt = $this->conn->prepare("SELECT *
                                         FROM games
                                         INNER JOIN user_games ON games.id = user_games.game_id
                                         WHERE user_games.user_id = :user_id;");
